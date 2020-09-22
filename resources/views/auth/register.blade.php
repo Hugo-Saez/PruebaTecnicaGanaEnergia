d36e4884243daf383b9f11e06822781dc8cec3d1
Hugo <hugo_s_c@hotmail.com>
2020-09-23 00:42:37 +0200

    modified register view to add door

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -68,6 +68,20 @@
                         </div>
 
                         <div class="form-group row">
+                            <label for="door" class="col-md-4 col-form-label text-md-right">{{ __('Puerta') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="door" type="text" class="form-control @error('door') is-invalid @enderror" name="door" value="{{ old('door') }}" required autocomplete="door" autofocus>
+
+                                @error('door')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Email') }}</label>
 
                             <div class="col-md-6">
