36e6005e1d0f3cde1c37a8c2026182d916e7afd2
Hugo <hugo_s_c@hotmail.com>
2020-09-23 14:37:30 +0200

    modified user model to add new fields

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'surnames', 'address', 'number', 'door', 'postal_code', 'locality', 'email', 'password',
     ];
 
     /**
